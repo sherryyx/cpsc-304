bf9e87386f832e972e30d607fddbb9a03fac75fa
Sherry Xie <sherryxy.71@gmail.com>
2020-03-30 23:51:56 -0700

    WIP

--- petbnb/routes/index.js
+++ petbnb/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const store = require('../store')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/home', function(req, res, next) {
+  console.log("home");
+  store.getPetsOfPetOwner(1).then((data) => {
+    console.log(data);
+    res.render('homepageowner', { title: "hello" });
+  })
 });
 
 module.exports = router;
