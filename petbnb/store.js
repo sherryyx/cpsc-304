b2d07437395baf15e4d2d914cf3c5b2daa8736db
Sherry Xie <sherryxy.71@gmail.com>
2020-04-03 21:35:28 -0700

    create separate pet sitter profile view without ability to review for search list

--- petbnb/store.js
+++ petbnb/store.js
@@ -7,7 +7,7 @@ const getPetsOfPetOwner = (petowner_id) => {
     WHERE p.user_id = ${petowner_id};`);
 }
 
-const createPetOwner = ({full_name, email_address, phone_number, house_number, street_name, postal_code, user_type}) => {
+const createPetOwner = ({full_name, phone_number, house_number, street_name, postal_code}) => {
     return knex.raw(`INSERT INTO petOwner (phonenumber, name, housenumber, street, postalcode)
     VALUES (
       '${phone_number}',
@@ -21,6 +21,17 @@ const createPetOwner = ({full_name, email_address, phone_number, house_number, s
 const getPetOwner = ({user_id}) => {
     return knex.raw(`SELECT * FROM petOwner
     where user_id = ${user_id};`);
+}
+
+const editPetOwner = ({full_name, phone_number, house_number, street_name, postal_code}, user_id) => {
+    return knex.raw(`UPDATE petOwner SET
+    name = '${full_name}',
+    phonenumber = ${phone_number},
+    housenumber = ${house_number},
+    street = '${street_name}',
+    postalcode = '${postal_code}'
+    WHERE user_id = ${user_id}
+    returning *;`);
 }
 
 // Pet queries
@@ -199,5 +210,6 @@ module.exports = {
     serviceType,
     getExperiencedSitters,
     insertBooking,
-    getPetInfo
-};
+    getPetInfo,
+    editPetOwner
+}
