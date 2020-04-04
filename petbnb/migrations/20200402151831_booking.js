cdac87360f303d8b7fb24393b1a1dca838505f87
Sherry Xie <sherryxy.71@gmail.com>
2020-04-04 11:30:21 -0700

    WIP

--- petbnb/migrations/20200402151831_booking.js
+++ petbnb/migrations/20200402151831_booking.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
     return knex.raw(`CREATE TABLE booking (
         booking_id serial,
         duration float,
+        totalPrice float,
         service_id integer NOT NULL,
         petOwner_id integer NOT NULL,
         pet_id integer NOT NULL,
